181121860ea61d41af0028e336feac5eefed5b05
FdongFdong <abnormal5626@naver.com>
2022-07-24 23:26:45 +0900

    수정

--- migrations/2_contract_migration.js
+++ migrations/2_contract_migration.js
@@ -1,20 +1,8 @@
-const MyContract = artifacts.require('MyContract');
 const KIP7Mock = artifacts.require('KIP7Mock');
-const Dex = artifacts.require('Dex');
 
 const KSP_MAINNET = '0xc6a2ad8cc6e4a7e08fc37cc5954be07d499e7654';
 const KSP_CONTRACT = '0xeff2bf4df47259333a5935AFD89e49Ff6865c1C1';
 // console.log(MyContract);
-module.exports = async function (deployer, accounts) {
-  // let temp = await KIP7Mock(KSP_MAINNET);
-  let temp = deployer.deploy(KIP7Mock, 'KlaySwap', 'KSP').then((result) => {
-    console.log(result);
-  });
-  await deployer.deploy(Dex, {
-    from: accounts[0],
-    value: '1000000000000000000',
-  });
-  const dex = await Dex.deployed();
-  // const KSP = await KIP7Mock.at(KSP_MAINNET);
-  // deployer.deploy(KIP7Mock(KIP7Mock.address, 'a'));
+module.exports = async function (deployer) {
+  const KSP = await new KIP7Mock(KSP_MAINNET, 1);
 };
